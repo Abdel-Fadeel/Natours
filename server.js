73e9a060af609b106b648133e3db2bd9deb4fe38
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-03-14 18:20:02 +0200

    EsLint

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const dotenv = require('dotenv');
-dotenv.config({
-  path: './config.env',
-});
 
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// console.log(process.env);
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
 });
