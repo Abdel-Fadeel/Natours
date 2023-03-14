73cd40e075ce10d0ffd586be6d106ff82eb1bc61
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-03-14 16:08:24 +0200

    environment variables

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use(express.static(`${__dirname}/public`));
 
