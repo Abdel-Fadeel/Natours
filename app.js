2673c662d494d6289f3978936b4b6521f82764db
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-03-14 15:27:06 +0200

    serving static files

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
-
 app.use(morgan('dev'));
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
