51f272d6a1a94c838f2b3fe714b51e0bfe15e618
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-03-22 16:28:25 +0200

    creating a tour model

--- server.js
+++ server.js
@@ -15,7 +15,25 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then((con) => console.log(con.connections));
+  .then(() => console.log('Connected successfully!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
