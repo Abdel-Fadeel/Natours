49cb37022816aa8b2b316be657d9f20762370210
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-03-22 21:38:35 +0200

    create documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,4 @@
-const fs = require('fs');
 const Tour = require('../models/tourModel');
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price)
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  next();
-};
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -21,33 +11,32 @@ exports.getAllTours = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  // const newId = tours[tours.length - 1].id + 1;
-  // const newTour = { id: newId, ...req.body };
-  // tours.push(newTour);
-  // fs.writeFile(
-  //   `${__dirname}/dev-data/data/tours-simple.json`,
-  //   JSON.stringify(tours),
-  //   (err) => {
-  //     res.status(201).json({
-  //       status: 'success',
-  //       data: {
-  //         tour: newTour,
-  //       },
-  //     });
-  //   }
-  // );
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
 
 exports.getTour = (req, res) => {
-  const tourId = req.params.id * 1;
+  // const tourId = req.params.id * 1;
   // const tour = tours.find((el) => el.id === tourId);
 
   res.status(200).json({
     status: 'success',
-    // data: {
-    //   tour,
-    // },
+    data: {
+      // tour,
+    },
   });
 };
 
