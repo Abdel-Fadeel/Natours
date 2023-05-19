6a284063ad43d1c53aaef5ac0436fbcc2992b4b2
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-05-19 03:27:32 +0300

    add password logic

--- models/userModel.js
+++ models/userModel.js
@@ -1,28 +1,43 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please tell us your name!'],
+    required: [true, 'Please tell us your name'],
   },
   email: {
     type: String,
-    required: [true, 'Please provide us your email!'],
+    required: [true, 'Please provide us your email'],
     unique: true,
     lowercase: true,
-    validate: [validator.isEmail, 'Please provide us a valid email!'],
+    validate: [validator.isEmail, 'Please provide us a valid email'],
   },
   photo: String,
   password: {
     type: String,
-    required: [true, 'Please provide a password!'],
+    required: [true, 'Please provide a password'],
     minLength: 8,
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your password!'],
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE OR SAVE !!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  // NOT persist passwordConfirm in the database
+  this.passwordConfirm = undefined;
 });
 
 module.exports = mongoose.model('user', userSchema);
