d6ef91508eff80691d727cc81a9b399cdad430c2
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-07-02 09:56:30 +0300

    add handler factory (read)

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,10 +1,49 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.createOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.create(req.body);
     res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const filter = {};
+    if (req.params.tourId) filter.tour = req.params.tourId;
+    // BUILD Query
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    // EXECUTE Query
+    const docs = await features.query;
+    // SEND Response
+    res.status(200).json({
+      status: 'success',
+      results: docs.length,
+      data: {
+        data: docs,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+
+    const doc = await query;
+    if (!doc) return next(new AppError(`No document found with that ID`, 404));
+
+    res.status(200).json({
       status: 'success',
       data: {
         data: doc,
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,26 +2,13 @@ const catchAsync = require('../utils/catchAsync');
 const Review = require('../models/reviewModel');
 const factory = require('./handlerFactory');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const filter = {};
-  if (req.params.tourId) filter.tour = req.params.tourId;
-
-  const reviews = await Review.find(filter);
-
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
-
 exports.setTourUserIds = catchAsync(async (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
 });
 
+exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
