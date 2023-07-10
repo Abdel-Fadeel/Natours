8571ff14ad35687ab163d5495d249579c80ac991
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-07-10 20:09:03 +0300

    build tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -10,8 +10,13 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    select: 'review user rating -tour -_id',
+  });
   res.status(200).render('tour', {
     title: 'The Forest Hiker',
+    tour,
   });
-};
+});
