15bb01af5ad4cc282d12790658f5ada1242509a7
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-08-05 13:15:43 +0300

    fix bug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -57,8 +57,8 @@ const createBookingCheckout = async (session) => {
   console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  // const price = session.line_items[0].unit_amount / 100;
-  await Booking.create({ tour, user, price: 200 });
+  const price = session.amount_total / 100;
+  await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
